d18f89d424cfb987f17a0832284f8b3c77e674b3
md-hasan-ali <hasanaliringku@gmail.com>
2024-01-29 16:53:27 +0600

    Integrate HTML report

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 const webpack = require("@cypress/webpack-preprocessor");
 const {
   addCucumberPreprocessorPlugin,
@@ -7,6 +8,7 @@ const {
 async function setupNodeEvents(on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
   await addCucumberPreprocessorPlugin(on, config);
+  allureWriter(on, config);
 
   on(
     "file:preprocessor",
