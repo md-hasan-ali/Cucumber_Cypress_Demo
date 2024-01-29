09082b69806ef224835be3597c0c1dfbc0a2b1fc
md-hasan-ali <hasanaliringku@gmail.com>
2024-01-29 17:37:45 +0600

    Some changes and uninstall unnecessary plugins

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require("cypress");
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 const webpack = require("@cypress/webpack-preprocessor");
 const {
   addCucumberPreprocessorPlugin,
@@ -8,7 +7,6 @@ const {
 async function setupNodeEvents(on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
   await addCucumberPreprocessorPlugin(on, config);
-  allureWriter(on, config);
 
   on(
     "file:preprocessor",
